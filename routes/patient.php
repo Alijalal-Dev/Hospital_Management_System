dc9af78563d232d764500f92fd092918337e667d
Alijalal-king <120683259+Alijalal-king@users.noreply.github.com>
2024-05-03 20:55:41 +0100

    working on dashboard patient and fixing errors on completed invoices ray and laboratoire employee

--- routes/patient.php
+++ routes/patient.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Dashboard_Doctor\PatientDetailsController;
 use App\Http\Controllers\Dashboard_Ray_Employee\InvoiceController;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use App\Http\Controllers\Dashboard_Patient\PatientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,6 +35,19 @@ Route::group(
         return view('Dashboard.dashboard_patient.dashboard');
     })->middleware(['auth:patient'])->name('dashboard.patient');
     //################################ end dashboard patient #####################################
+
+    Route::middleware(['auth:patient'])->group(function () {
+
+        //############################# patients route ##########################################
+        Route::get('invoices', [PatientController::class,'invoices'])->name('invoices.patient');
+        Route::get('laboratories', [PatientController::class,'laboratories'])->name('laboratories.patient');
+        Route::get('view_laboratories/{id}', [PatientController::class,'viewLaboratories'])->name('laboratories.view');
+        Route::get('rays', [PatientController::class,'rays'])->name('rays.patient');
+        Route::get('view_rays/{id}', [PatientController::class,'viewRays'])->name('rays.view');
+        Route::get('payments', [PatientController::class,'payments'])->name('payments.patient');
+        //############################# end patients route ######################################
+
+    });
 
 
     require __DIR__ . '/auth.php';
