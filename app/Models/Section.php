75d8600c0fe66c09ccd1b654e408ecbf2cbc3406
Alijalal-king <120683259+Alijalal-king@users.noreply.github.com>
2024-07-11 21:43:33 +0100

    Create appointments livewire frontend

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -5,17 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Translatable;
-use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 
 class Section extends Model
 {
     use Translatable; // 2. To add translation methods
+    use HasFactory;
     protected $fillable =['name','description'];
     // 3. To define which attributes needs to be translated
     public $translatedAttributes = ['name','description'];
-    use HasFactory;
+
     public function doctors()
     {
         return $this->hasMany(Doctor::class);
     }
+
 }
--- database/seeders/DoctorTableSeeder.php
+++ database/seeders/DoctorTableSeeder.php
@@ -13,14 +13,17 @@ class DoctorTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $doctors =  Doctor::factory()->count(20)->create();
-        $Appointments = Appointment::all();
-
-
-        Doctor::all()->each(function ($doctor) use ($Appointments) {
-           $doctor->doctorappointments()->attach(
-              $Appointments->random(rand(1,7))->pluck('id')->toArray()
-           );
-       });
+        Doctor::factory()->count(30)->create();
+        //        $Appointments = Appointment::all();
+        //
+        ////        foreach ($doctors as $doctor){
+        ////            $Appointments = Appointment::all()->random()->id;
+        ////            $doctor->doctorappointments()->attach($Appointments);
+        ////        }
+        //        Doctor::all()->each(function ($doctor) use ($Appointments) {
+        //           $doctor->doctorappointments()->attach(
+        //              $Appointments->random(rand(1,7))->pluck('id')->toArray()
+        //           );
+        //       });
     }
 }
