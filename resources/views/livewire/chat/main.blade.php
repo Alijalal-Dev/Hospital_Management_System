d9d9ba9e64f51a4ada7c07204b45a5b5a203168e
Alijalal-Dev <aj0546006@gmail.com>
2024-08-28 14:13:44 +0100

    wokring on the dashboard of the doctor

--- resources/views/livewire/chat/main.blade.php
+++ resources/views/livewire/chat/main.blade.php
@@ -4,7 +4,7 @@
     <!-- breadcrumb -->
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
-            <div class="d-flex"><h4 class="content-title mb-0 my-auto">المحادثات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ المحادثات الاخيرة</span></div>
+            <div class="d-flex"><h4 class="content-title mb-0 my-auto">{{ trans('messages.Conversations') }}</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/  {{ trans('messages.Recent Conversations') }}</span></div>
         </div>
     </div>
     <!-- breadcrumb -->
@@ -16,7 +16,7 @@
             <div class="card">
                 <div class="main-content-left main-content-left-chat">
                     <nav class="nav main-nav-line main-nav-line-chat">
-                        <a class="nav-link active" data-toggle="tab" href="">المحادثات الاخيرة</a>
+                        <a class="nav-link active" data-toggle="tab" href="">{{ trans('messages.Recent Conversations') }}</a>
                     </nav>
                     @livewire('chat.chatlist')
                 </div>
