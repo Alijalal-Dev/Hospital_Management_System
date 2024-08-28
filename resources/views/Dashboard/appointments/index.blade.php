7330bbb39dc4fba90ff9a60fdc94b70e1811cddc
Alijalal-Dev <aj0546006@gmail.com>
2024-08-28 11:19:26 +0100

    appointment folder and traduction

--- resources/views/Dashboard/appointments/index.blade.php
+++ resources/views/Dashboard/appointments/index.blade.php
@@ -1,21 +1,20 @@
 @extends('Dashboard.layouts.master')
 @section('css')
     <link rel="stylesheet" href="{{ URL::asset('Dashboard/plugins/sumoselect/sumoselect-rtl.css') }}">
-    <link href="{{ URL::asset('dashboard/plugins/notify/css/notifIt.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('Dashboard/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('Dashboard/plugins/amazeui-datetimepicker/css/amazeui.datetimepicker.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('Dashboard/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.css') }}"
-        rel="stylesheet">
-    <link href="{{ URL::asset('Dashboard/plugins/pickerjs/picker.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('Dashboard/plugins/spectrum-colorpicker/spectrum.css') }}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/plugins/notify/css/notifIt.css')}}" rel="stylesheet"/>
+    <link href="{{URL::asset('Dashboard/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('Dashboard/plugins/amazeui-datetimepicker/css/amazeui.datetimepicker.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('Dashboard/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('Dashboard/plugins/pickerjs/picker.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('Dashboard/plugins/spectrum-colorpicker/spectrum.css')}}" rel="stylesheet">
 @endsection
 @section('page-header')
     <!-- breadcrumb -->
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">المواعيد</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمة
-                    المواعيد</span>
+                <h4 class="content-title mb-0 my-auto">{{ trans('appointment.appointments') }}</h4>
+                <span class="text-muted mt-1 tx-13 mr-2 mb-0">/ {{ trans('appointment.appointment_list') }}</span>
             </div>
         </div>
     </div>
@@ -23,13 +22,12 @@
 @endsection
 @section('content')
     @include('Dashboard.messages_alert')
-    <!-- row opened -->
     @if (session('error'))
         <div class="alert alert-danger">
             {{ session('error') }}
         </div>
     @endif
-
+    <!-- row opened -->
     <div class="row row-sm">
         <!--div-->
         <div class="col-xl-12">
@@ -38,35 +36,36 @@
                     <div class="table-responsive">
                         <table class="table text-md-nowrap" id="example1">
                             <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>اسم المريض</th>
-                                    <th>البريد الالكتروني</th>
-                                    <th>القسم</th>
-                                    <th>الدكتور</th>
-                                    <th>الهاتف</th>
-                                    <th>ملاحظات</th>
-                                    <th>العمليات</th>
-                                </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>{{ trans('appointment.patient_name') }}</th>
+                                <th>{{ trans('appointment.email') }}</th>
+                                <th>{{ trans('appointment.section') }}</th>
+                                <th>{{ trans('appointment.doctor') }}</th>
+                                <th>{{ trans('appointment.phone') }}</th>
+                                <th>{{ trans('appointment.notes') }}</th>
+                                <th>{{ trans('appointment.actions') }}</th>
+                            </tr>
                             </thead>
                             <tbody>
-                                @foreach ($appointments as $appointment)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $appointment->name }}</a></td>
-                                        <td>{{ $appointment->email }}</td>
-                                        <td>{{ $appointment->section->name }}</td>
-                                        <td>{{ $appointment->doctor->name }}</td>
-                                        <td>{{ $appointment->phone }}</td>
-                                        <td>{{ $appointment->notes }}</td>
-                                        <td>
-                                            <button class="btn btn-sm btn-success" data-toggle="modal"
-                                                data-target="#approval{{ $appointment->id }}"><i class="fas fa-check"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    @include('Dashboard.appointments.approval')
-                                @endforeach
+                            @foreach($appointments as $appointment)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>{{$appointment->name}}</td>
+                                    <td>{{$appointment->email}}</td>
+                                    <td>{{$appointment->section->name}}</td>
+                                    <td>{{$appointment->doctor->name}}</td>
+                                    <td>{{$appointment->phone}}</td>
+                                    <td>{{$appointment->notes}}</td>
+                                    <td>
+                                        <button class="btn btn-sm btn-success" data-toggle="modal"
+                                                data-target="#approval{{$appointment->id}}"><i class="fas fa-check"></i>
+                                        </button>
+                                     
+                                    </td>
+                                </tr>
+                                 @include('Dashboard.appointments.approval')
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -89,26 +88,25 @@
     <!--Internal Sumoselect js-->
     <script src="{{ URL::asset('Dashboard/plugins/sumoselect/jquery.sumoselect.js') }}"></script>
     <!--Internal  Notify js -->
-    <script src="{{ URL::asset('dashboard/plugins/notify/js/notifIt.js') }}"></script>
-    <script src="{{ URL::asset('/plugins/notify/js/notifit-custom.js') }}"></script>
-
+    <script src="{{URL::asset('dashboard/plugins/notify/js/notifIt.js')}}"></script>
+    <script src="{{URL::asset('/plugins/notify/js/notifit-custom.js')}}"></script>
 
     <!--Internal  Datepicker js -->
-    <script src="{{ URL::asset('dashboard/plugins/jquery-ui/ui/widgets/datepicker.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/jquery-ui/ui/widgets/datepicker.js')}}"></script>
     <!--Internal  jquery.maskedinput js -->
-    <script src="{{ URL::asset('dashboard/plugins/jquery.maskedinput/jquery.maskedinput.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/jquery.maskedinput/jquery.maskedinput.js')}}"></script>
     <!--Internal  spectrum-colorpicker js -->
-    <script src="{{ URL::asset('dashboard/plugins/spectrum-colorpicker/spectrum.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/spectrum-colorpicker/spectrum.js')}}"></script>
     <!-- Internal Select2.min js -->
-    <script src="{{ URL::asset('dashboard/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/select2/js/select2.min.js')}}"></script>
     <!--Internal Ion.rangeSlider.min js -->
-    <script src="{{ URL::asset('dashboard/plugins/ion-rangeslider/js/ion.rangeSlider.min.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/ion-rangeslider/js/ion.rangeSlider.min.js')}}"></script>
     <!--Internal  jquery-simple-datetimepicker js -->
-    <script src="{{ URL::asset('dashboard/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js')}}"></script>
     <!-- Ionicons js -->
-    <script src="{{ URL::asset('dashboard/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.js')}}"></script>
     <!--Internal  pickerjs js -->
-    <script src="{{ URL::asset('dashboard/plugins/pickerjs/picker.min.js') }}"></script>
+    <script src="{{URL::asset('dashboard/plugins/pickerjs/picker.min.js')}}"></script>
     <!-- Internal form-elements js -->
-    <script src="{{ URL::asset('dashboard/js/form-elements.js') }}"></script>
+    <script src="{{URL::asset('dashboard/js/form-elements.js')}}"></script>
 @endsection
